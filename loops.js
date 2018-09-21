f01afb1d515263578795ca31215f59ae6d9e4c6d
JESSIE ANDERSON <jessanderson222@gmail.com>
2018-09-21 18:19:39 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,9 +17,21 @@ function whileLoop (n) {
     return 'done'
   }
   
+<<<<<<< HEAD
 function doWhileLoop(array) {
   do {
     array.shift();
   } while (array.length > 0);
   return array;
+=======
+  function doWhileLoop(array) {
+    function incrementVariable() {
+  i = i + 1;
+}
+ 
+do {
+  console.log("doo-bee-doo-bee-doo");
+  incrementVariable();
+} while (i < 5);
+>>>>>>> 9c9ba9d249eb24419e99cd0dfd6ad56ab0187b53
   }
